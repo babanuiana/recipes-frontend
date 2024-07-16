659e71cb68eced15bbbbb2430ff7632be1fa9899
Babanu Iana <babanu.iana@gmail.com>
2024-07-16 21:59:51 +0200

    created new component - Profile

---
+++ components/auth/Profile.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="wrapper">
+    <BaseTypography class="profile-name" variant="body-01-medium">
+      {{ userName }}
+    </BaseTypography>
+
+    <BaseCircularIconButton
+      icon="carbon:user-avatar-filled"
+      size="small"
+      variant="primary"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+type Props = {
+  userName: string | undefined;
+};
+
+withDefaults(defineProps<Props>(), {
+  userName: "",
+});
+</script>
+<style Lang="scss" scoped>
+.wrapper {
+  display: flex;
+  align-items: center;
+  .profile-name {
+    margin-right: 8px;
+  }
+}
+</style>
